b3df5cf18b3a96eacb0bc9dbcd35c43a4bc520ea
bhumipatel1894 <48902835+bhumipatel1894@users.noreply.github.com>
2026-03-16 23:03:39 -0400

    changes in page.js

--- yourshop/src/app/order/page.js
+++ yourshop/src/app/order/page.js
@@ -1,29 +1,35 @@
 "use client";
-import React from 'react'
+
+export const dynamic = "force-dynamic";
+
 import React, { useEffect, useState } from "react";
 
-function page() {
+function Page() {
+  const [orderDetails, setOrderDetails] = useState(null);
 
-   const [orderDetails, setOrderDetails] = useState(null);
-   useEffect(() => {
-    const Orderdata = localStorage.getItem("Order");
-    if (Orderdata) {
-      setOrderDetails(JSON.parse(Orderdata));
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const Orderdata = localStorage.getItem("Order");
+      if (Orderdata) {
+        setOrderDetails(JSON.parse(Orderdata));
+      }
     }
   }, []);
 
   if (!orderDetails) {
     return <div className="mt-28 text-center">Loading...</div>;
   }
-   
+
   return (
     <section className="min-h-screen items-center text-center justify-center bg-gray-50 mt-28">
-       <h2 className="text-2xl font-bold mb-4">Thank you {orderDetails.fullName} for your order!</h2>
-       <p className="text-gray-700">
-         Your order has been placed successfully. We will process it soon and send you a confirmation email with the details. If you have any questions, please contact our support team.
-       </p>
+      <h2 className="text-2xl font-bold mb-4">
+        Thank you {orderDetails.fullName} for your order!
+      </h2>
+      <p className="text-gray-700">
+        Your order has been placed successfully. We will process it soon.
+      </p>
     </section>
-  )
+  );
 }
 
-export default page
+export default Page;
