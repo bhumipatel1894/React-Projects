9dff67ba0657eafdd53b7c164ea6a203cc7e532b
bhumipatel1894 <48902835+bhumipatel1894@users.noreply.github.com>
2026-03-16 22:53:01 -0400

    order page added useeffect hook

--- yourshop/src/app/order/page.js
+++ yourshop/src/app/order/page.js
@@ -1,10 +1,21 @@
 "use client";
 import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function page() {
 
+   const [orderDetails, setOrderDetails] = useState(null);
+   useEffect(() => {
     const Orderdata = localStorage.getItem("Order");
-    const orderDetails = JSON.parse(Orderdata);
+    if (Orderdata) {
+      setOrderDetails(JSON.parse(Orderdata));
+    }
+  }, []);
+
+  if (!orderDetails) {
+    return <div className="mt-28 text-center">Loading...</div>;
+  }
+   
   return (
     <section className="min-h-screen items-center text-center justify-center bg-gray-50 mt-28">
        <h2 className="text-2xl font-bold mb-4">Thank you {orderDetails.fullName} for your order!</h2>
